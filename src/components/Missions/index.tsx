38030b3d0d603dac49b4f4c1871ca666e4b2e7c1
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-22 17:44:24 -0300

    Rendering the list with the space missions

--- src/components/Missions/index.tsx
+++ src/components/Missions/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import missions from '../../data/missions';
+import MissionCard from '../MissionCard';
 import Title from '../Title';
 import { Container } from './styles';
 
@@ -6,6 +8,15 @@ function Missions() {
   return (
     <Container>
       <Title headline="Missões" />
+      {missions.map((mission) => (
+        <MissionCard
+          key={mission.name}
+          name={mission.name}
+          year={Number(mission.year)}
+          country={mission.country}
+          destination={mission.destination}
+        />
+      ))}
     </Container>
   );
 }
