4ba34b13940f874caafaedab3d91fe3391dddff3
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-22 19:35:59 -0300

    Fix error image path

--- src/styles.ts
+++ src/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background-image: url('/public/background-galaxy.png');
+  background-image: url('/background-galaxy.png');
   background-position: center; /* Center the image */
   background-repeat: no-repeat; /* Do not repeat the image */
   background-size: cover; /* Resize the background image to cover the entire container */
