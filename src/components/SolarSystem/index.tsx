24a7e7342a39b7915e3779bd23329ae1f8581d27
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-22 16:43:10 -0300

    Rendering list with the planets

--- src/components/SolarSystem/index.tsx
+++ src/components/SolarSystem/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Planets from '../../data/planets';
+import PlanetCard from '../PlanetCard';
 import Title from '../Title';
 import { Container } from './styles';
 
@@ -6,6 +8,13 @@ function SolarSystem() {
   return (
     <Container>
       <Title headline="Planetas" />
+      {Planets.map((planet) => (
+        <PlanetCard
+          key={planet.name}
+          planetName={planet.name}
+          planetImage={planet.image}
+        />
+      ))}
     </Container>
   );
 }
